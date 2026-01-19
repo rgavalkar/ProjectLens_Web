415a9c928e62fe4d6676fa3eb5085bcd03f04f76
rgavalkar <rgavalkar@trikaiser.com>
2026-01-19 16:40:40 +0530

    RG-19/01/2026-01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 export const routes: Routes = [
-  { path: '', component: ProjectListComponent }
+  { path: '', component: ProjectListComponent },            // Project list page
+  { path: 'project/:id', component: ProjectDetailsComponent } // Project details page
 ];
-
-
